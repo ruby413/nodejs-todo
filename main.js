18de676fb4a88c74852a0211e94b573f1e025bcb
LeeJuyeon <ruby413@hanmail.net>
2019-06-05 18:30:58 +0900

    fixed : use promis for terminal question

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const Login = require('./login');
+const login = new Login();
 const readline = require('readline');
 let r = readline.createInterface({
     input:process.stdin,
@@ -11,29 +12,57 @@ const order = (order, callback) => {
     r.on('line', callback)
 }
 
+class AskforLogin {
+    askName(){
+        return new Promise((resolve) => {
+            r.question("이름을 입력해주세요 : ", (name) => { resolve(name) })
+        })
+    }
+    askId(name){
+        return new Promise((resolve) => {
+            r.question("아이디를 입력해주세요 : ", (id) => { resolve([name, id]) })
+        })
+    }
+    askPw([name, id]){
+        return new Promise((resolve) => {
+            r.question("비밀번호를 입력해주세요 : ", (pw) => { resolve([name, id, pw]) })
+        })
+    }
+    signUp([name, id, pw]){
+        login.signUp(name +"," + id + "," + pw)
+    }
+
+    login([name, id, pw]){
+        console.log(`${id} 님 게임시작합니다.`)
+    }
+}
+
+const askforLogin = new AskforLogin();
 
 const startGame = () => {
-    order( "명령하세요 : ", (answer) => {doLogin(answer)} )
+    order( "명령하세요 : ", (answer) => {
+        if(answer === "회원가입"){
+            askforLogin.askName().then(askforLogin.askId).then(askforLogin.askPw).then(askforLogin.signUp)
+        }
+        if(answer === "로그인"){
+            askforLogin.askId().then(askforLogin.askPw).then(askforLogin.login)
+        }
+    } )
 }
-
-
-const doLogin = (answer) => {
-    const login = new Login();
-    if (answer === "s") {
-        order( "이름을 입력해주세요 : " , (name) => {login.signUp("name " + name)} );
-        // order( "아이디를 입력해주세요 : " , (id) => {login.signUp("id " + id)} );
-        // order( "비밀번호를 입력해주세요 : " , (pw) => {login.signUp("pw " + pw)} );
-        // Promise.all([
-        //     new Promise(resolve => order( "이름을 입력해주세요 : " , (name) => {login.signUp("name " + name)} )), // 1
-        //     new Promise(resolve => setTimeout(() => resolve(2), 2000)), // 2
-        //     new Promise(resolve => setTimeout(() => resolve(3), 1000))  // 3
-        //   ]).then(console.log) // [ 1, 2, 3 ]
-        //     .catch(console.log);
-        //login.signUp();
-    }
-    if (answer === "로그인") {
-        order( "아이디를 입력해주세요 : " , (answer) => {console.log(answer)} )
-    }
-}
-
+ 
 startGame()
+
+// const doLogin = (answer) => {
+//     const login = new Login();
+//     if (answer === "회원가입") {
+//         order( "이름을 입력해주세요 : " , (name) => {login.signUp("name " + name)} );
+//         // order( "아이디를 입력해주세요 : " , (id) => {login.signUp("id " + id)} );
+//         // order( "비밀번호를 입력해주세요 : " , (pw) => {login.signUp("pw " + pw)} );
+//         //login.signUp();
+//     }
+//     if (answer === "로그인") {
+//         order( "아이디를 입력해주세요 : " , (answer) => {console.log(answer)} )
+//     }
+// }
+
+// startGame()
