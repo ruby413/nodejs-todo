38f79aa6b102d6d069c3b4d607c415318d9b16e4
LeeJuyeon <ruby413@hanmail.net>
2019-06-06 22:23:44 +0900

    refactor : refactoring and change sync method when they give just status infomation

--- control_data.js
+++ control_data.js
@@ -6,21 +6,22 @@ class ControlData {
         const signUpColumn = "name,ID,PW" + "\n"
         const [name, ID, PW] = information
         let contents = name + "," + ID + "," + PW + "\n";
-        fs.exists('./data/client_data.csv', function (exists) { 
-            if(exists){
-                fs.appendFile('./data/client_data.csv', contents, function (err) { 
-                    if (err) throw err; 
-                });
-            }else{
-                fs.writeFile('./data/client_data.csv', signUpColumn + contents, 'utf8' , function(err) {
-                    if(err) throw err;
-                });
-            }
-        });
+        if( this.existDataFile() ) {
+            fs.appendFileSync('./data/client_data.csv', contents) 
+        }else{
+            fs.writeFileSync('./data/client_data.csv', signUpColumn + contents, 'utf8');
+        }
+        
+    }
+
+    existDataFile(){
+        return fs.existsSync('./data/client_data.csv');
     }
 
     readClientData(){
-       return fs.readFileSync('./data/client_data.csv', 'utf8');
+        if(this.existDataFile()){
+            return fs.readFileSync('./data/client_data.csv', 'utf8');
+        }
     }
 
 }
--- login.js
+++ login.js
@@ -3,37 +3,31 @@ const ControlData = require('./control_data');
 const data = new ControlData();
 
 class Login {
-    constructor() {
-      this.clientData = data.readClientData()
-    }
 
-    checkData(informationArray){
-      let checkArray = [];
-      informationArray.forEach((info)=>{
-        let checkData = this.clientData.match(new RegExp(info,'g'))
-        if(checkData !== null){
-          checkArray.push(checkData[0]);
-        }
-      })
-      if(JSON.stringify(informationArray) === JSON.stringify(checkArray)){
-        return true
-      }else{
-        return false
+  alreadyExistData(inputDataArray){
+    let clientData = data.readClientData()
+    let sameDataArray = [];
+    inputDataArray.forEach((info)=>{
+      let sameData = clientData.match(new RegExp(info,'g'))
+      if(sameData !== null){
+        sameDataArray.push(sameData[0]);
       }
-    }
+    })
+    return JSON.stringify(inputDataArray) === JSON.stringify(sameDataArray) ? true : false
+  }
 
-    signUp(information){
-      if(this.checkData(information)) {
-        console.log("이미 사용하고 계시는 정보입니다.")
-      }else{
-        data.makeClientData(information) 
-        console.log("회원가입이 완료되었습니다.")
-      }
+  signUp(inputDataArray){
+    if(!data.existDataFile() || !this.alreadyExistData(inputDataArray)){
+      data.makeClientData(inputDataArray) 
+      console.log("회원가입이 완료되었습니다.")
+    }else{
+      console.log("이미 사용하고 계시는 정보입니다.")
     }
+  }
 
-    checkLogin(information){
-      this.checkData(information) ? console.log("로그인 되었습니다.") : console.log("잘못입력하셨습니다.")
-    }
+  checkLogin(inputDataArray){
+    this.alreadyExistData(inputDataArray) ? console.log("로그인 되었습니다.") : console.log("잘못입력하셨습니다.")
+  }
 
     
   }
