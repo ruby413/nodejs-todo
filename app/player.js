6f1eb8d197a2e85bec1434fc49c7d39bf73b7c4c
LeeJuyeon <ruby413@hanmail.net>
2019-06-11 16:51:51 +0900

    create : shoot ship method

--- app/player.js
+++ app/player.js
@@ -2,32 +2,46 @@ const Setting = require('./setting');
 const Game = require('./game');
 const game = new Game;
 
+    /* 
+     * 0: 빈 자리
+     * size: size 
+     * -1 : 잠수함 부분 폭파
+     * -2 : 잠수함 격추
+     * -3 : 빈공간 격추
+     */
+
 class Player {
+
     constructor() {
         this.sea = game.sea;
-        this.ship = Setting.ships;    //[ 5, 4, 3, 3, 2 ]
+        this.ship = Setting.ships;    // [ 5, 4, 3, 3, 2 ]
+        this.shipInfo = {}
     }
 
+    // place ship
+
     placeShip(size, direction, x, y){
-        if(direction === "h" && this.checkShip(size, x, y)){
+        if(direction === "h" && this.checkShip(size, direction, x, y)){
             return this.placeHorizontalShip(size, x, y)  // return ship
-        }else if(direction === "v" && this.checkShip(size, x, y)){
+        }else if(direction === "v" && this.checkShip(size, direction, x, y)){
             return this.placeVerticalShip(size, x, y)   // return ship
         }
     }
 
-    checkShip(size, x, y){
-        this.sea.forEach((row, j)=>{
-            row.forEach((place, i)=>{
-                if(place === 1){
-                    for(let check = 0; check < size; check++){
-                        if(x+check === i+1 && y === j+1){
-                            throw "이미 배가 위치하고 있습니다."
-                        }
-                    }
+    checkShip(size, direction, x, y){
+        if(direction === "h"){
+            for(let i=0; i<size; i++){
+                if(this.sea[y-1][x-1+i] > 1){
+                    throw "이미 배가 위치하고 있습니다."
                 }
-            })
-        })
+            }
+        }else{
+            for(let i=0; i<size; i++){
+                if(this.sea[y-1+i][x-1] > 1){
+                    throw "이미 배가 위치하고 있습니다."
+                }
+            }
+        }
         return true
     } 
 
@@ -35,13 +49,14 @@ class Player {
         if(this.sea[y-1].length - x >= size){
             this.sea[y-1].forEach((empty, i)=>{
                 if((x-1)<= i && i< (x-1)+size){
-                    this.sea[y-1][i] = empty + 1
+                    this.sea[y-1][i] = empty + size;
+
                 }   
             })
         }else{
             this.sea[y-1].forEach((empty, i)=>{
                 if(i >= this.sea[y-1].length - size){
-                    this.sea[y-1][i] = empty + 1
+                    this.sea[y-1][i] = empty + size;
                 }   
             })
         } 
@@ -51,29 +66,46 @@ class Player {
     placeVerticalShip(size, x, y){
         if(this.sea.length - y >= size){
             this.sea.forEach((empty, i)=>{
-                if((y-1) <= i && i < (y-1)+size){
-                    this.sea[i][x-1] = empty[x-1] + 1;
+                if((y-1) <= i && i < (y-1) + size){
+                    this.sea[i][x-1] = empty[x-1] + size;
                 }
             })
        }else{
             this.sea.forEach((empty, i)=>{
                 if(i >= this.sea.length - size){
-                    this.sea[i][x-1] = empty[x-1] + 1;
+                    this.sea[i][x-1] = empty[x-1] + size;
                 }
             })
        }
        return this.sea;
     }
+
+
+    //shoot ship
+    shootShip(x, y){
+        let arr = this.sea[y-1];
+        if(this.sea[y-1][x-1] > 1) {
+            arr.splice(x-1, 1, -1);
+        }else{
+            arr.splice(x-1, 1, -3);
+            console.log("공간이 비어있습니다.")
+        }
+        return this.sea;
+    }
+
+    
 }
 
 const player = new Player;
 
 try {
-    console.log(player.placeShip(5, "h", 8, 3))
-    console.log(player.placeShip(5, "h", 7, 2))
-    console.log(player.placeShip(2, "h", 8, 2))
-  }
-  catch(e) {
+    console.log(player.placeShip(5, "h", 2, 3))
+    // console.log(player.placeShip(5, "h", 1, 3))
+    // console.log(player.placeShip(2, "v", 1, 2))
+    console.log(player.shootShip(2, 3))
+    console.log(player.shootShip(2, 4))
+}
+catch(e) {
     console.log(e);
-  }
-// console.log(player.placeHorizontalShip(3, 2, 3))
+}
+
